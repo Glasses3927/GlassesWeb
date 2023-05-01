1887f1109654c2b0f402b174e30c524d1c07b1f0
Glasses3927 <Glasses3927@users.noreply.github.com>
2023-05-01 12:27:59 +0000

    deploy: 3628b76d05124a9e450df312c93f1c77214d0039

--- js/real.js
+++ js/real.js
@@ -1,14 +1,24 @@
-var param = location.search;
-
-if(param == '?hoge=hoge'){
-    document.location = "../real/";
-} else {
-    var url = document.referrer;
-    param = '?' + url.split('?')[1]
-    if(param != '?hoge=hoge'){
-        document.location = "../login/";
-    }
+const sha256 = async (text) => {    //SHA-256によるハッシュ化
+    const uint8  = new TextEncoder().encode(text)
+    const digest = await crypto.subtle.digest('SHA-256', uint8)
+    return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2,'0')).join('')
 }
+
+var param = location.search;
+sha256(param).then(hash => {
+    if(hash == '4d6ef576cb7bcab08f69612cf92b27f73817e6063ed147ea1d370ee68ac47ff3'){
+        document.location = "../real/";
+    } else {
+        var url = document.referrer;
+        param = '?' + url.split('?')[1]
+        sha256(param).then(hash => {
+            if(hash != '4d6ef576cb7bcab08f69612cf92b27f73817e6063ed147ea1d370ee68ac47ff3'){
+                document.location = "../login/";
+            }
+        });
+    }    
+});
+
 
 
 //ローディング後フェードアウト
