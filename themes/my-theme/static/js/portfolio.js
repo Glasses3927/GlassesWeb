0ebc827a17479e37fca15fda963ce908de494a93
Glässes <42023384+Glasses3927@users.noreply.github.com>
2023-05-29 23:00:28 +0900

    Add files via upload

--- themes/my-theme/static/js/portfolio.js
+++ themes/my-theme/static/js/portfolio.js
@@ -16,8 +16,20 @@ csvfunc();
 
 //Instagram表示
 
-const url_enc = 'U2FsdGVkX18IBOVC/JxnyX7tvjZY5LT8EAtb6GMx9PhUj2Q+bGUy1hb5K/Xt04i5nAmHYmrE0j3pYzKIxHHv3rm2j98n/WMZwJCnxnQQI366QVLR1s2F2pAwVUZDrsjyxrpSYOx/TbptaefBxUiBEYbaP/QkdKsfJabbHilO4WvLhnkiTJPru6n8rOaCWvFIKetynbgt91NrECm39rq0GHJ3/bcvqRvKZ0Tvb3otRvxACIsHGIJkDb+SHCHjNbbrvR7ZD5tlV3PXUb7KoAMPoMZwnZ9KGvQa8GyGXoCiniPSo1iRSiZnwhCJChjbbZASm6Vsw4Ym79YOQhuRE37I5f9xGlmXC8dj/R7PabWCbRg=';
+const url_enc = 'U2FsdGVkX18kAsXdzDTcoUkp8gsEd6DIGpYe8HHsQ8aMiGdWub5unJQ7J+EZB8QksLk91XTUerdm3/MuQmZ253JjPR7LUsqtD9EQFW4RISflnDGQ363DfrVOtbqSsuwUYNmLrip+3xlUnSAEkfW18plc+P+1ZyDyP+uB0LO/vUrL8OtZQzuR4ZjsAyANGkR0PbX7ETXQnBmQak4dAjl/XGyQXjzEB7FfwDvRHNK6IGC+ZUgRUMg8xCs0Ej/5pr3Ckgj3g/0vE1wU7EwPJC7hc+BuaIbeI153kNGaCfQFw3sjQuNv86+wzqfMCaWRoSWTvULme3r++VF2Mg23ty8mEFGq1A+yrpdsviSTJktGwQtLpLEKZN3GVNLjFRRbMYUu';
+//'U2FsdGVkX18IBOVC/JxnyX7tvjZY5LT8EAtb6GMx9PhUj2Q+bGUy1hb5K/Xt04i5nAmHYmrE0j3pYzKIxHHv3rm2j98n/WMZwJCnxnQQI366QVLR1s2F2pAwVUZDrsjyxrpSYOx/TbptaefBxUiBEYbaP/QkdKsfJabbHilO4WvLhnkiTJPru6n8rOaCWvFIKetynbgt91NrECm39rq0GHJ3/bcvqRvKZ0Tvb3otRvxACIsHGIJkDb+SHCHjNbbrvR7ZD5tlV3PXUb7KoAMPoMZwnZ9KGvQa8GyGXoCiniPSo1iRSiZnwhCJChjbbZASm6Vsw4Ym79YOQhuRE37I5f9xGlmXC8dj/R7PabWCbRg=';
 const url_dec = CryptoJS.AES.decrypt(url_enc, "pass").toString(CryptoJS.enc.Utf8);
+
+function loop(insta){
+	let url = insta.media_url; // 投稿メディアのURLを取得
+	let href = insta.permalink; // 投稿URLを取得
+	let caption = insta.caption; //　投稿のキャプションを取得
+	if(url.indexOf('.mp4') <= 0){ // .mp4以外
+		$('.portfolio-list').append(`
+<img src="${url}" alt="${caption}" onclick="gotoinsta('${href}')" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
+		`);
+	}
+}
 
 (function ($) {
     $.ajax({  // jQueryのajaxでjsonデータを取得
@@ -25,15 +37,15 @@ const url_dec = CryptoJS.AES.decrypt(url_enc, "pass").toString(CryptoJS.enc.Utf8
         url: url_dec,
         dataType: 'json',　
         success: function (json) {
-            var insta = json.data;
-            for (var i = 0; i < json.data.length; i++) {
-            	let url = insta[i].media_url; // 投稿メディアのURLを取得
-        		let href = insta[i].permalink; // 投稿URLを取得
-            	let caption = insta[i].caption; //　投稿のキャプションを取得
-            	if(url.indexOf('.mp4') <= 0){ // .mp4以外
-            		$('.portfolio-list').append(`
-<img src="${url}" alt="${caption}" onclick="gotoinsta('${href}')" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
-            		`);
+            var alldata = json.data;
+            for (var i=0; i<alldata.length; i++) {
+				if(alldata[i].children != null) {
+					var childdata = alldata[i].children.data;
+					for (var j=0; j<childdata.length; j++) {
+						loop(childdata[j]);
+					}
+				} else {
+					loop(alldata[i]);
 				}
             }
         }
